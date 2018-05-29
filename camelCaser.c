4cb3b1b51b920ff708886732fd7620a243bd21e8
kartikagrawal27 <kartikagrawal27@gmail.com>
2018-05-29 15:43:55 -0500

    Initial Commit

---
+++ camelCaser.c
@@ -0,0 +1,12 @@
+#include "camelCaser.h"
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+char **camel_caser(const char *input_str) 
+{	
+	if(input_str==NULL)
+    	return NULL;
+    
+}
